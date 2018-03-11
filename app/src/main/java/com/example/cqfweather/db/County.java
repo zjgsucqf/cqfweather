14609917c6e522e657009802fd934eb241b084b2
carey <zzili_cqf@163.com>
2018-03-11 10:26:31 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/cqfweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.cqfweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Carey on 2018/3/11.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String CountyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
