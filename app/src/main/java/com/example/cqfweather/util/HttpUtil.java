f60f2d8b4aebfeb01c6ace5589a141689d284ab0
carey <zzili_cqf@163.com>
2018-03-11 12:07:42 +0800

    完成遍历省市县三级列表的功能.

---
+++ app/src/main/java/com/example/cqfweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.cqfweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Carey on 2018/3/11.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
